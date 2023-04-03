23e1e20b4ce2b26ec4f5eceaa0d54f7fed8bba4f
Aniruddha Sinha <asinha0493@gmail.com>
2023-04-03 14:28:11 +0530

    added terraform cloud config

--- versions.tf
+++ versions.tf
@@ -1,7 +1,14 @@
 terraform {
-  backend "gcs" {
-    bucket = "odin-admin-state-bkt"
-    prefix = "terraform/platform-state"
+  # backend "gcs" {
+  #   bucket = "odin-admin-state-bkt"
+  #   prefix = "terraform/platform-state"
+  # }
+
+  cloud {
+    organization = "asinha0493"
+    workspaces {
+      name = "gcp-platform-parent"
+    }
   }
   required_providers {
     google = {
